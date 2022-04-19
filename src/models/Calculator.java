ab04b476776f6fc2255ec59c6d747e88f40ef052
Daniel jaramillo <daniel.jaramillo01@globant.com>
2022-04-19 18:51:42 -0500

    Add multiply and divide methods

--- src/models/Calculator.java
+++ src/models/Calculator.java
@@ -3,4 +3,11 @@ package models;
 public class Calculator {
     public static int sum(int a, int b) { return a + b; };
     public static int subtract(int a, int b) { return a - b; };
+    public static int multiply(int a, int b) {
+        return a * b;
+    };
+
+    public static int divide(int a, int b) {
+        return a / b;
+    };
 }
--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -6,5 +6,13 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println(Calculator.sum(4, 3));
         System.out.println(Calculator.subtract(4, 3));
+        System.out.println(Calculator.multiply(5,0));
+        System.out.println(Calculator.multiply(5,2));
+        System.out.println(Calculator.divide(15, 3));
+        try {
+            System.out.println(Calculator.divide(5, 0));
+        } catch (Exception e) {
+            System.out.println("Please not divide by 0, thank you.");
+        }
     }
 }
